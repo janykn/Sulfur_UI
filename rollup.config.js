dfe20291c4e9070bcce9ec939ca343f191c96d56
Janardhan Ykunta <150250506+JanardhanYkunta@users.noreply.github.com>
2024-06-03 20:09:57 +0530

    Sulfur Begins: Starter commit

---
+++ rollup.config.js
@@ -0,0 +1,34 @@
+import babel from 'rollup-plugin-babel';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import postcss from 'rollup-plugin-postcss';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/index.es.js',
+      format: 'esm',
+      sourcemap: true,
+    }
+  ],
+  plugins: [
+    peerDepsExternal(),
+    babel({
+      exclude: 'node_modules/**',
+      presets: ['@babel/preset-env', '@babel/preset-react'],
+    }),
+    nodeResolve(),
+    commonjs(),
+    postcss({
+      extensions: ['.css']
+    })
+  ],
+  external: ['react', 'react-dom']
+};
