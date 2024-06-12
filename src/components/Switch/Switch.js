fe4070ded4a83561c8751f627990290a0e4da819
Janardhan Ykunta <150250506+JanardhanYkunta@users.noreply.github.com>
2024-06-12 11:48:30 +0530

    Bug Fix: Conflicting Element selectors in switch replaced with class-id selectors

--- src/components/Switch/Switch.js
+++ src/components/Switch/Switch.js
@@ -6,21 +6,23 @@ function Switch({  onToggle, isOn, backgroundColor }) {
 
   return (
     <div className='switch__container'>
-    <input
+      <input
+        className='switch__input'
         type='checkbox'
         id='dark-mode-toggle'
         checked={isOn}
         onChange={onToggle}
       />
-      <label 
-        htmlFor='dark-mode-toggle' 
+      <label
+        className='switch__toggle'
+        htmlFor='dark-mode-toggle'
         style={{ background: backgroundColor }}
       >
-          <svg class="moon" version="1.0" width="80px" height="80px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M3.32031 11.6835C3.32031 16.6541 7.34975 20.6835 12.3203 20.6835C16.1075 20.6835 19.3483 18.3443 20.6768 15.032C19.6402 15.4486 18.5059 15.6834 17.3203 15.6834C12.3497 15.6834 8.32031 11.654 8.32031 6.68342C8.32031 5.50338 8.55165 4.36259 8.96453 3.32996C5.65605 4.66028 3.32031 7.89912 3.32031 11.6835Z" stroke="#000000b3" stroke-width="0.8" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>
+        <svg className="switch__moon" version="1.0" width="80px" height="80px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M3.32031 11.6835C3.32031 16.6541 7.34975 20.6835 12.3203 20.6835C16.1075 20.6835 19.3483 18.3443 20.6768 15.032C19.6402 15.4486 18.5059 15.6834 17.3203 15.6834C12.3497 15.6834 8.32031 11.654 8.32031 6.68342C8.32031 5.50338 8.55165 4.36259 8.96453 3.32996C5.65605 4.66028 3.32031 7.89912 3.32031 11.6835Z" stroke="#000000b3" strokeWidth="0.8" strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
 
-<svg class ="sun" version="1.0" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+        <svg className="switch__sun" version="1.0" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
 	 width="80px" height="80px" viewBox="0 0 64 64" enable-background="new 0 0 64 64">
 <g>
 	<path fill-rule="evenodd" clip-rule="evenodd" fill="#231F20" d="M32,14.002c-9.941,0-18,8.059-18,18s8.059,18,18,18
