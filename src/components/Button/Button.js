f0674f954423014a9c9e594537ac90d8b9de0fb0
Janardhan Ykunta <150250506+JanardhanYkunta@users.noreply.github.com>
2024-06-30 13:53:36 +0530

    MotionGrads, Spring Props and Hover Props Decoupling

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,37 +2,36 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { motion } from 'framer-motion';
 
-const Button = ({ onClick, children, style, spring }) => {
-  const defaultStyles = {
-    bg: 'white', // Default background color
-    color: 'black',
-    border: 'none',
-    radius: '4px',
-    pad: '6px 16px',
-    shadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
-    hoverBg: '#f0f0f0',
-    hoverColor: '#000',
-  };
-
-  // Merge provided styles with default styles
-  const mergedStyles = { ...defaultStyles, ...style };
-
+const Button = ({
+  onClick,
+  children,
+  bg,
+  color,
+  border,
+  radius,
+  pad,
+  shadow,
+  hoverBg,
+  hoverColor,
+  spring,
+  hover,
+}) => {
   const buttonStyles = {
     display: 'inline-flex',
     flexWrap: 'no-wrap',
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    background: mergedStyles.bg,
-    color: mergedStyles.color,
-    border: mergedStyles.border,
-    borderRadius: mergedStyles.radius,
-    padding: mergedStyles.pad,
-    cursor: 'pointer',
-    boxShadow: mergedStyles.shadow,
     userSelect: 'none',
     WebkitTapHighlightColor: 'transparent',
     outline: 'none',
+    cursor: 'pointer',
+    background: bg,
+    color: color,
+    border: border,
+    borderRadius: radius,
+    padding: pad,
+    boxShadow: shadow,
     '@media (max-width: 600px)': {
       padding: '4px 8px',
       fontSize: '14px',
@@ -44,13 +43,23 @@ const Button = ({ onClick, children, style, spring }) => {
     userSelect: 'none',
   };
 
-  const ButtonComponent = spring ? motion.div : 'div';
+  const ButtonComponent = motion.div;
+
+  let hoverScale = spring ? 1.1 : 1;
+  let tapScale = spring ? 0.9 : 1;
+
+  let hoverProps = {
+    scale: hoverScale,
+    background: hover ? hoverBg : bg,
+    color: hover ? hoverColor : color,
+  };
+  let tapProps = { scale: tapScale };
 
   return (
     <ButtonComponent
-      whileHover={spring ? { scale: 1.1, background: mergedStyles.hoverBg, color: mergedStyles.hoverColor } : null}
-      whileTap={spring ? { scale: 0.9 } : null}
-      transition={spring ? { bounceDamping: 10, bounceStiffness: 600 } : {}}
+      whileHover={hoverProps}
+      whileTap={tapProps}
+      transition={{ bounceDamping: 10, bounceStiffness: 600 }}
       style={buttonStyles}
       onClick={onClick}
     >
@@ -64,31 +73,93 @@ const Button = ({ onClick, children, style, spring }) => {
 Button.propTypes = {
   onClick: PropTypes.func.isRequired,
   children: PropTypes.node.isRequired,
-  style: PropTypes.shape({
-    bg: PropTypes.string,
-    color: PropTypes.string,
-    border: PropTypes.string,
-    radius: PropTypes.string,
-    pad: PropTypes.string,
-    shadow: PropTypes.string,
-    hoverBg: PropTypes.string,
-    hoverColor: PropTypes.string,
-  }),
+  bg: PropTypes.string,
+  color: PropTypes.string,
+  border: PropTypes.string,
+  radius: PropTypes.string,
+  pad: PropTypes.string,
+  shadow: PropTypes.string,
+  hoverBg: PropTypes.string,
+  hoverColor: PropTypes.string,
   spring: PropTypes.bool,
+  hover: PropTypes.bool,
 };
 
 Button.defaultProps = {
-  style: {
-    bg: 'white', // Default background color
-    color: 'black',
-    border: 'none',
-    radius: '4px',
-    pad: '6px 16px',
-    shadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
-    hoverBg: '#f0f0f0',
-    hoverColor: '#000',
-  },
-  spring: true, // Default to enabling spring animations
+  bg: 'white', 
+  color: 'black',
+  border: '1px solid rgba(255, 255, 255, 0.1)',
+  radius: '4px',
+  pad: '15px 45px',
+  shadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
+  hoverBg: '#f0f0f0',
+  hoverColor: '#000',
+  spring: true,
+  hover: true,
 };
 
-export default Button;
+const GradientButton = ({
+  onClick,
+  children,
+  motionGrad,
+  gradSpring,
+  gradHover,
+  hoverColor = 'white',
+}) => {
+  const buttonStyles = {
+    margin: '10px',
+    padding: '15px 45px',
+    textAlign: 'center',
+    transition: '0.5s',
+    backgroundSize: '200% auto',
+    color: 'white',
+    boxShadow: '0 0 2px #eee',
+    borderRadius: '10px',
+    display: 'block',
+    backgroundImage: `linear-gradient(to right, ${motionGrad[0]} 0%, ${motionGrad[1]} 51%, ${motionGrad[2]} 100%)`,
+    cursor: 'pointer',
+  };
+
+  let hoverProps = {};
+  let tapProps = {};
+
+  if (gradHover) {
+    hoverProps.backgroundPosition = 'right center';
+    hoverProps.color = hoverColor;
+    hoverProps.textDecoration = 'none';
+  }
+
+  if (gradSpring) {
+    hoverProps.scale = 1.1;
+    tapProps.scale = 0.9;
+  }
+
+  return (
+    <motion.div
+      style={buttonStyles}
+      whileHover={hoverProps}
+      whileTap={tapProps}
+      transition={{ bounceDamping: 10, bounceStiffness: 600 }}
+      onClick={onClick}
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+GradientButton.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired,
+  motionGrad: PropTypes.arrayOf(PropTypes.string).isRequired,
+  gradSpring: PropTypes.bool,
+  gradHover: PropTypes.bool,
+  hoverColor: PropTypes.string,
+};
+
+GradientButton.defaultProps = {
+  gradSpring: false,
+  gradHover: true,
+  hoverColor: 'white',
+};
+
+export { Button, GradientButton };
