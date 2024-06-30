96736dc5cdd584233a1326274cdced4a7a43bdf8
Janardhan Ykunta <150250506+JanardhanYkunta@users.noreply.github.com>
2024-06-30 13:54:58 +0530

    MotionGrads, Spring Props and Hover Props Decoupling: Commit 2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-export { default as Button } from './components/Button/Button';
+export { Button, GradientButton } from './components/Button/Button';
 export { default as InputBox } from './components/InputBox/InputBox';
 export {default as Modal} from './components/Modal/Modal';
 export {default as Dropdown} from './components/Dropdown/Dropdown';
